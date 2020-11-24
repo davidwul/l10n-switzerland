ef751b9b2fd4a24282ca280c2467e1bdf27fb7ea
David Wulliamoz <dwulliamoz@compassion.ch>
2020-11-24 04:19:02 +0000

    85a93e806e5b41883375082d5036f63c2dc78e5c

--- l10n_ch_pain_base/models/account_move_line.py
+++ l10n_ch_pain_base/models/account_move_line.py
@@ -2,7 +2,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import models, api
-
+import logging
+_logger = logging.getLogger(__name__)
 
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
@@ -11,8 +12,13 @@ class AccountMoveLine(models.Model):
     def _prepare_payment_line_vals(self, payment_order):
         vals = super()._prepare_payment_line_vals(payment_order)
         if self.invoice_id and self.invoice_id._is_isr_reference():
-            vals['local_instrument'] = 'CH01'
-            vals['communication_type'] = 'isr'
+            _logger.info('PREPARE '+self.invoice_id.partner_bank_id.sanitized_acc_number[4:6])
+            if self.invoice_id.partner_bank_id.sanitized_acc_number[4:6] in ['30', '31'] \
+            and self.invoice_id.partner_bank_id.acc_type == 'iban':
+                vals['communication_type'] = 'qrr'
+            else:
+                vals['local_instrument'] = 'CH01'
+                vals['communication_type'] = 'isr'
             if vals['communication']:
                 vals['communication'] = vals['communication'].replace(' ', '')
         return vals
